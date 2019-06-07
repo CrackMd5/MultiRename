999f028b2d6043c5c09da0ecbdfdf76349b483e9
Md5-Descrypter <45376821+CrackMd5@users.noreply.github.com>
2019-06-08 01:35:51 +0800

    Add files via upload

---
+++ MultiRename/Form1.cs
@@ -0,0 +1,76 @@
+﻿using OCR.TesseractWrapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace MultiRename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string d = textBox_Path.Text;
+            if (!Directory.Exists(d))
+            {
+                MessageBox.Show("目录" + d + "不存在");
+                return;
+            }
+            if (!d.EndsWith("\\")) d += "\\";
+            string d1 = d + "new\\";
+            if (!Directory.Exists(d1))
+                Directory.CreateDirectory(d1);
+            Regex reg = new Regex(@"\d+\.\d\d", RegexOptions.Compiled);
+            foreach (string f in Directory.GetFiles(textBox_Path.Text))
+            {
+                if (!f.ToLower().EndsWith(".png") && !f.ToLower().EndsWith(".jpg") && !f.ToLower().EndsWith(".bmp")) continue;
+                Bitmap bmp = new Bitmap(f);
+                TesseractProcessor process = new TesseractProcessor();
+                process.SetPageSegMode(ePageSegMode.PSM_SINGLE_LINE);
+                process.Init(System.Environment.CurrentDirectory + "\\", "chi_sim", (int)eOcrEngineMode.OEM_DEFAULT);
+                try
+                {
+                    string result = process.Recognize(bmp);
+                    Match m = reg.Match(result);
+                    if (m.Success)
+                    {
+                        string amount = m.ToString();
+                        File.Copy(f, d1 + amount + Path.GetExtension(f), true);
+                    }
+                    else
+                    {
+                        File.Copy(f, d1 + Path.GetFileName(f), true);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("识别文件出错:" + f);
+                    File.Copy(f, d1 + Path.GetFileName(f), true);
+                }
+                bmp.Dispose();
+            }
+            System.Diagnostics.Process.Start(d1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox_Path.Text = System.IO.Path.GetDirectoryName(ofd.FileName);
+            }
+        }
+    }
+}
---
+++ MultiRename/bin/Debug/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MultiRename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_Path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "改名";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_Path
+            // 
+            this.textBox_Path.Location = new System.Drawing.Point(87, 28);
+            this.textBox_Path.Name = "textBox_Path";
+            this.textBox_Path.Size = new System.Drawing.Size(387, 20);
+            this.textBox_Path.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "图片路径：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(494, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 168);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Path);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "支付宝微信收款二维码批量改名";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_Path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
